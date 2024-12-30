1beba38717048befa28821e6ee152a6d4cf9f26c
Kimbeomju <kbj52264291@gmail.com>
2024-12-30 15:54:35 +0900

    폰트 최적화, 앱 아이콘 변경, 로컬라이징

--- P1/Assets/@Scripts/Controller/Creature/Hero.cs
+++ P1/Assets/@Scripts/Controller/Creature/Hero.cs
@@ -143,6 +143,8 @@ public class Hero : Creature
             Managers.Object.SpawnGameObject(Target.CenterPosition, "Object/Effect/Explosion/HeroAttackEffect");
 
             Target.GetComponent<IDamageable>().OnDamaged(this);
+            Managers.Sound.Play(ESound.Effect, "Sounds/Sword", 0.4f);
+
         }
         CreatureState = ECreatureState.Move;
     }
